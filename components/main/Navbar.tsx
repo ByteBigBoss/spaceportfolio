069d85dd3f971709af03f4b92eae109a3907f7ce
ByteBigBoss <nethminas42@gmail.com>
2024-01-24 17:09:53 +0530

    changes ++

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -7,16 +7,8 @@ const Navbar = () => {
     <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]  bg-[#03001417] backdrop-blur-md z-50 px-10 '>
       <div className='w-full h-full flex items-center justify-between m-auto px-[10px]'>
         <a href="#about-me" className='h-auto w-auto flex items-center'>
-          <Image 
-          src='/NavLogo.png' 
-          alt='logo'
-          width={70}
-          height={70}
-          className='cursor-pointer hover:animate-slowspin'
-          />
-          <span className='font-bold ml-[10px] hidden md:block text-gray-300'>
-            WebChain Dev
-          </span>
+         <div className='text-[21px] font-bold text-white'>NS</div>
+     
         </a>
 
         <div className='w-[500px] h-full flex items-center justify-between md:mr-20'>
